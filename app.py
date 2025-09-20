ff7acfb21961adaf9d88f61c66120ad0056ea2b2
Vaib <vaibosee@gmail.com>
2025-09-20 22:13:39 +0530

    Initial commit

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('Main.html')
 
 @app.route('/upload_template', methods=['POST'])
 def upload_template():
@@ -81,4 +81,6 @@ def generate_reports():
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+
+app.run(debug=True, port=5050)
 
